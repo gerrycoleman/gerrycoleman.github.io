6132e90346439ba86452e44939bcda1cd81dbfd2
Gerry Coleman <gerrycolemann@icloud.com>
2024-07-13 12:59:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let name = "userName"
+console.log(name)
+console.log("output in console from test.js file ")
+
+{
+    userName: "thanodexs",
+    age: 25,
+    gender: "Male",
+    location: "Nairobi",
+}
